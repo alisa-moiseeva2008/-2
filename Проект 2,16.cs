536ccb61ad1d01c22a845a83ebf8f8110b10c477
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:00:50 +0400

    Update Проект 2,16.cs

--- Проект 2,16.cs
+++ Проект 2,16.cs
@@ -1,18 +1,16 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите первое число: ");
-            double x = double.Parse(Console.ReadLine());            
-            Console.Write("Введите второе число: ");
-            double y = double.Parse(Console.ReadLine());           
-            double arithmeticMean = (x + y) / 2;
-            double geometricMean = Math.Pow(x * y, 1.0 / 2.0);         
-            Console.WriteLine($"Среднее арифметическое: {arithmeticMean}");
-            Console.WriteLine($"Среднее геометрическое: {geometricMean}");            
-            Console.ReadKey();
-        }
+        Console.Write("Введите первое число: ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        double y = double.Parse(Console.ReadLine());
+        double arithmeticMean = (x + y) / 2;
+        double geometricMean = Math.Pow(x * y, 1.0 / 2.0);
+        Console.WriteLine($"Среднее арифметическое: {arithmeticMean}");
+        Console.WriteLine($"Среднее геометрическое: {geometricMean}");
+        Console.ReadKey();
     }
 }
