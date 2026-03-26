401a6c404cd05b5722d8b12961ff45beabb2ca43
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 19:54:43 +0400

    Update Проект 2,9.cs

--- Проект 2,9.cs
+++ Проект 2,9.cs
@@ -1,14 +1,12 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите число: ");
-            double c = double.Parse(Console.ReadLine());       
-            double rounded = Math.Round(c, 2);
-            Console.WriteLine($"Число с точностью до сотых: {rounded}");           
-            Console.ReadKey();
-        }
+        Console.Write("Введите число: ");
+        double c = double.Parse(Console.ReadLine());
+        double rounded = Math.Round(c, 2);
+        Console.WriteLine($"Число с точностью до сотых: {rounded}");
+        Console.ReadKey();
     }
 }
