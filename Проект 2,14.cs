dcd65e0abf6fbbf33bb0595cd2d7e210a5bb2d94
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 19:57:23 +0400

    Update Проект 2,14.cs

--- Проект 2,14.cs
+++ Проект 2,14.cs
@@ -1,11 +1,9 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("7  15  100");          
-            Console.ReadKey();
-        }
+        Console.WriteLine("7  15  100");
+        Console.ReadKey();
     }
 }
