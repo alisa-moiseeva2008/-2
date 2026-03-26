1698352d3565b9bf67fd56021a505a329d5cae8c
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 19:13:16 +0400

    Update Проект 2,1-4.cs

--- Проект 2,1-4.cs
+++ Проект 2,1-4.cs
@@ -1,15 +1,13 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Random rnd = new Random();         
-            Console.WriteLine("Случайные числа:");
-            Console.WriteLine(rnd.Next()); 
-            Console.WriteLine(rnd.Next(5)); 
-            Console.WriteLine(rnd.Next(-5, 5));         
-            Console.ReadKey();
-        }
+        Random rnd = new Random();
+        Console.WriteLine("Случайные числа:");
+        Console.WriteLine(rnd.Next());
+        Console.WriteLine(rnd.Next(5));
+        Console.WriteLine(rnd.Next(-5, 5));
+        Console.ReadKey();
     }
 }
