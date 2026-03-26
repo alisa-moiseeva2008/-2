7b8053ff118a88c2f851749c4f48879dd7550eac
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:00:08 +0400

    Update Проект 2,15.cs

--- Проект 2,15.cs
+++ Проект 2,15.cs
@@ -1,15 +1,13 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            float z; 
-            int a = 1, b = 2, c = 3;
-            const int m = 3;          
-            z = (float)(a + b + c) / m;  
-            Console.WriteLine($"Результат: {z}");           
-            Console.ReadKey();
-        }
+        float z;
+        int a = 1, b = 2, c = 3;
+        const int m = 3;
+        z = (float)(a + b + c) / m;
+        Console.WriteLine($"Результат: {z}");
+        Console.ReadKey();
     }
 }
