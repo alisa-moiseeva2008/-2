c55b4c82b737460af7755ff05cba56a7a459fd4b
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:01:31 +0400

    Update Проект 2,17.cs

--- Проект 2,17.cs
+++ Проект 2,17.cs
@@ -1,16 +1,14 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите массу тела (m): ");
-            double m = double.Parse(Console.ReadLine());           
-            Console.Write("Введите объем тела (V): ");
-            double V = double.Parse(Console.ReadLine());            
-            double p = m / V;            
-            Console.WriteLine($"Плотность материала: {p}");           
-            Console.ReadKey();
-        }
+        Console.Write("Введите массу тела (m): ");
+        double m = double.Parse(Console.ReadLine());
+        Console.Write("Введите объем тела (V): ");
+        double V = double.Parse(Console.ReadLine());
+        double p = m / V;
+        Console.WriteLine($"Плотность материала: {p}");
+        Console.ReadKey();
     }
 }
