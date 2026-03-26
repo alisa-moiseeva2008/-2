956599fd9e9a44bf3b2a12138882a513daa22350
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:04:13 +0400

    Update Проект 2,22.cs

--- Проект 2,22.cs
+++ Проект 2,22.cs
@@ -1,17 +1,15 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите катет a: ");
-            double a = double.Parse(Console.ReadLine());        
-            Console.Write("Введите катет b: ");
-            double b = double.Parse(Console.ReadLine());    
-            double c = Math.Sqrt(a * a + b * b);
-            double P = a + b + c;
-            Console.WriteLine($"Периметр = {P}");      
-            Console.ReadKey();
-        }
+        Console.Write("Введите катет a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Введите катет b: ");
+        double b = double.Parse(Console.ReadLine());
+        double c = Math.Sqrt(a * a + b * b);
+        double P = a + b + c;
+        Console.WriteLine($"Периметр = {P}");
+        Console.ReadKey();
     }
 }
