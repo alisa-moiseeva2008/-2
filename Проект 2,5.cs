4d8ae1b0cfac42c4d3b85b4f2d5d3ca5eb9ecfd9
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 19:13:55 +0400

    Update Проект 2,5.cs

--- Проект 2,5.cs
+++ Проект 2,5.cs
@@ -1,18 +1,16 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Random rnd = new Random();           
-            Console.Write("Введите начало диапазона a: ");
-            int a = int.Parse(Console.ReadLine());
-            
-            Console.Write("Введите конец диапазона b: ");
-            int b = int.Parse(Console.ReadLine());           
-            int randomNumber = rnd.Next(a, b + 1);
-            Console.WriteLine($"Случайное число из диапазона [{a}, {b}]: {randomNumber}");           
-            Console.ReadKey();
-        }
+        Random rnd = new Random();
+        Console.Write("Введите начало диапазона a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите конец диапазона b: ");
+        int b = int.Parse(Console.ReadLine());
+        int randomNumber = rnd.Next(a, b + 1);
+        Console.WriteLine($"Случайное число из диапазона [{a}, {b}]: {randomNumber}");
+        Console.ReadKey();
     }
 }
