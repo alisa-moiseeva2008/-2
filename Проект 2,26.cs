63d1bee5ab2620cd78d6d6c9373ff45fa5a530e4
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 19:52:26 +0400

    Update Проект 2,26.cs

--- Проект 2,26.cs
+++ Проект 2,26.cs
@@ -1,29 +1,27 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int a, b, c;
-            a = b = c = 2;
-            double z = a * b + c++ / b - a * b * --c;
-            Console.WriteLine($"Исходное выражение (a=2, b=2, c=2): {z}");
-            a = b = c = 2;
-            double z1 = (a * b + c++) / (b - a) * b * --c;
-            Console.WriteLine($"а) Результат 3.66: {z1}");
-            a = b = c = 2;
-            double z2 = a * b + c++ / (b - a) * b * --c;
-            Console.WriteLine($"б) Результат -1: {z2}");
-            a = b = c = 2;
-            double z3 = a * (b + c++) / b - a * b * --c;
-            Console.WriteLine($"в) Результат 2: {z3}");
-            a = b = c = 2;
-            double z4 = a * b + c++ / (b - a * b * --c);
-            Console.WriteLine($"г) Результат -2: {z4}");
-            a = b = c = 2;
-            double z5 = a * (b + c++) / (b - a) * b * --c;
-            Console.WriteLine($"д) Результат -4: {z5}");        
-            Console.ReadKey();
-        }
+        int a, b, c;
+        a = b = c = 2;
+        double z = a * b + c++ / b - a * b * --c;
+        Console.WriteLine($"Исходное выражение (a=2, b=2, c=2): {z}");      
+        a = b = c = 2;
+        double z1 = (a * b + c++) / (b - a) * b * --c;
+        Console.WriteLine($"а) Результат 3.66: {z1:F2}");   
+        a = b = c = 2;
+        double z2 = a * b + c++ / (b - a) * b * --c;
+        Console.WriteLine($"б) Результат -1: {z2}");  
+        a = b = c = 2;
+        double z3 = a * (b + c++) / b - a * b * --c;
+        Console.WriteLine($"в) Результат 2: {z3}");  
+        a = b = c = 2;
+        double z4 = a * b + c++ / (b - a * b * --c);
+        Console.WriteLine($"г) Результат -2: {z4}"); 
+        a = b = c = 2;
+        double z5 = a * (b + c++) / (b - a) * b * --c;
+        Console.WriteLine($"д) Результат -4: {z5}");
+        Console.ReadKey();
     }
 }
