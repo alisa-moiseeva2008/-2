67ee332d8fe490df628479ec32c51ff3e112d33f
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 19:14:34 +0400

    Update Проект 2,6.cs

--- Проект 2,6.cs
+++ Проект 2,6.cs
@@ -1,15 +1,13 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            float a = 3.2f;
-            decimal b = 2.6m;
-            double c = 4.5;
-            c = a + (double)b + c;            
-            Console.WriteLine($"Результат: {c}");
-            Console.ReadKey();
-        }
+        float a = 3.2f;
+        decimal b = 2.6m;
+        double c = 4.5;
+        c = a + (double)b + c;
+        Console.WriteLine($"Результат: {c}");
+        Console.ReadKey();
     }
 }
