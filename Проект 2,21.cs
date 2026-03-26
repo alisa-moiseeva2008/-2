80e106817b20c24bf616de67bc633848da194ec5
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:03:49 +0400

    Update Проект 2,21.cs

--- Проект 2,21.cs
+++ Проект 2,21.cs
@@ -1,16 +1,14 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите внешний радиус R: ");
-            double R = double.Parse(Console.ReadLine());        
-            Console.Write("Введите внутренний радиус r: ");
-            double r = double.Parse(Console.ReadLine());      
-            double S = Math.PI * (R * R - r * r);      
-            Console.WriteLine($"Площадь кольца = {S}");     
-            Console.ReadKey();
-        }
+        Console.Write("Введите внешний радиус R: ");
+        double R = double.Parse(Console.ReadLine());
+        Console.Write("Введите внутренний радиус r: ");
+        double r = double.Parse(Console.ReadLine());
+        double S = Math.PI * (R * R - r * r);
+        Console.WriteLine($"Площадь кольца = {S}");
+        Console.ReadKey();
     }
 }
