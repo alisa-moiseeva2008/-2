fe95eb4a89b694ee8cac2c93ef9cdd76513294b9
alisa-moiseeva2008 <alisadima2008@gmail.com>
2026-03-26 20:04:36 +0400

    Update Проект 2,23.cs

--- Проект 2,23.cs
+++ Проект 2,23.cs
@@ -1,21 +1,19 @@
 using System;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите первое число: ");
-            double n1 = double.Parse(Console.ReadLine());           
-            Console.Write("Введите второе число: ");
-            double n2 = double.Parse(Console.ReadLine());           
-            double abs1 = Math.Abs(n1);
-            double abs2 = Math.Abs(n2);           
-            double arithmeticMean = (abs1 + abs2) / 2;
-            double geometricMean = Math.Sqrt(abs1 * abs2);       
-            Console.WriteLine($"Модули чисел: {abs1} и {abs2}");
-            Console.WriteLine($"Среднее арифметическое модулей: {arithmeticMean}");
-            Console.WriteLine($"Среднее геометрическое модулей: {geometricMean}");         
-            Console.ReadKey();
-        }
+        Console.Write("Введите первое число: ");
+        double n1 = double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        double n2 = double.Parse(Console.ReadLine());
+        double abs1 = Math.Abs(n1);
+        double abs2 = Math.Abs(n2);
+        double arithmeticMean = (abs1 + abs2) / 2;
+        double geometricMean = Math.Sqrt(abs1 * abs2);
+        Console.WriteLine($"Модули чисел: {abs1} и {abs2}");
+        Console.WriteLine($"Среднее арифметическое модулей: {arithmeticMean}");
+        Console.WriteLine($"Среднее геометрическое модулей: {geometricMean}");
+        Console.ReadKey();
     }
 }
